78ce7ce4f713e385c1f859383a66253a44b22c20
Astrid <astghik.piliposyan@gmail.com>
2015-07-07 22:17:10 -0700

    fixing $wrapper issue

--- numberify.js
+++ numberify.js
@@ -1,19 +1,21 @@
+var id;
+
 function numberify(selector) {
 
-    var wrapperId = '#numberify_wrapper',
-    	lineNumbersId = "#n_numbers",
-    	style = {
-        	width: '30px',
-        	marginRight: '5px',
-        	paddingRight: '5px',
-        	lineHeight: 1
-    	}
-		allowedInnerTags = ["p", "div"],
-		content = '';
+    var wrapperId = 'numberify_wrapper',
+        lineNumbersId = 'n_numbers',
+        allowedInnerTags = ["p", "div"],
+        style = {
+            width: '30px',
+            marginRight: '5px',
+            paddingRight: '5px',
+            lineHeight: 1
+        },
+        content = '';
 
     content = $(selector).html();
     $(selector).text('').html("<div id=" + wrapperId + ">" + content + "</div>")
-    $(wrapperId).parent().prepend('<div id=' + lineNumbersId + ' ></div>');
+    $('#' + wrapperId).parent().prepend('<div id=' + lineNumbersId + '></div>');
 
     //function to get actual line numbers not the css property value
     function getRealLineHeight(selector) {
@@ -32,14 +34,13 @@ function numberify(selector) {
 
         if ($(selector).has(tag).length > 0) {
 
-            $(selector + ' ' + tag).not(lineNumbersId).css({
+            $(selector + ' ' + tag).not('#' + lineNumbersId).css({
                 paddingTop: 0,
                 paddingBottom: 0,
                 marginTop: line_height,
                 marginBottom: line_height
             });
-        } 
-        else {
+        } else {
             return;
         }
     }
@@ -47,7 +48,6 @@ function numberify(selector) {
     function getRows(selector) {
 
         line_height = parseFloat(getRealLineHeight(selector));
-
         style.lineHeight = line_height;
 
         $.each(allowedInnerTags, function(index) {
@@ -56,14 +56,14 @@ function numberify(selector) {
 
         var height = $(selector).height();
         var rows = Math.floor(height / line_height);
+
         return rows;
     }
 
     function addNumbers(selector) {
-        
-        var count = getRows(wrapperId);
-       
-        $(lineNumbersId).html('').css({
+        var count = getRows('#' + wrapperId);
+
+        $('#' + lineNumbersId).css({
             "width": style.width,
             "margin-right": style.marginRight,
             "padding-right": style.paddingRight,
@@ -71,7 +71,7 @@ function numberify(selector) {
         });
 
         for (var i = 1; i <= count; i++) {
-            $(lineNumbersId).append('<div>' + i + '</div>');
+            $('#' + lineNumbersId).append('<div>' + i + '</div>');
         };
 
     }
